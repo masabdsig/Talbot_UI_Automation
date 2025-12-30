1adc58fd72968cbc3a5892839039a99528afa4ce
SumitDsig <smishra@dsig-inc.com>
2025-12-30 20:05:31 +0530

    final forgot password

--- tests/TC01_login.spec.js
+++ tests/TC01_login.spec.js
@@ -36,7 +36,7 @@ test.describe('Login scenarios', () => {
         expect(page.url()).toContain('/dashboard');
     });
 
-    test.only('TC04 - Check Forgot Password Flow', async ({ page }) => {
+    test('TC04 - Check Forgot Password Flow', async ({ page }) => {
         const login = new LoginPage(page);
         const testEmail = process.env.TEST_EMAIL || 'test@example.com';
         const senderEmail = process.env.SENDER_EMAIL || 'admin@example.com';
